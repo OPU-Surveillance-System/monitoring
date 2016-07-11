b0f4678878d5c53db254ea7f747116375c94d5d9
henriojord <henriojord@eisti.eu>
2016-07-12 00:20:54 +0900

    greedy planning

--- master/scripts/planner/planner.py
+++ master/scripts/planner/planner.py
@@ -7,6 +7,7 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from tqdm import tqdm
+import operator
 path.append("..")
 
 import settings
@@ -17,10 +18,6 @@ class SimulatedAnnealingPlanner(Annealer):
     """
 
     def __init__(self, state, mapper, nb_drone):
-        """
-        Initialize the solver
-        """
-
         self.state = state
         self.mapper = mapper
         self.nb_drone = nb_drone
@@ -98,7 +95,72 @@ class GreedyPlanner:
     """
     """
 
-def plot_plan(plan, show=True):
+    def __init__(self, state, mapper, nb_drone):
+        """
+        """
+
+        self.state = state
+        self.mapper = mapper
+        self.nb_drone = nb_drone
+        self.targets = mapper.default_targets
+        self.mapped_paths = self.mapper.world
+        self.battery_plan = [0 for d in range(self.nb_drone)]
+        self.plan = [[] for d in range(self.nb_drone)]
+
+    def find_closest_point(self, point):
+        """
+        """
+
+        points = [self.mapper.paths[(point, t)] for t in self.targets]
+        points = sorted(points, key=operator.itemgetter(1))
+
+        return points[0]
+
+    def compute_plan(self):
+        """
+        """
+
+        for d in range(self.nb_drone):
+            while self.battery_plan[d] <= settings.MAX_BATTERY_UNIT:
+                closest_target = self.find_closest_point(self.state[d][len(self.state[d]) - 2])
+                n = closest_target[0][len(closest_target[0]) - 1]
+                n = tuple(reversed(n))
+                if self.battery_plan[d] + self.mapper.paths[(self.state[d][len(self.state[d]) - 2], n)][1] + self.mapper.paths[(n, self.state[d][len(self.state[d]) - 1])][1] < settings.MAX_BATTERY_UNIT:
+                    self.state[d].insert(len(self.state[d]) - 1, n)
+                    self.battery_plan[d] += self.mapper.paths[(self.state[d][len(self.state[d]) - 2], n)][1] + self.mapper.paths[(n, self.state[d][len(self.state[d]) - 1])][1]
+                    self.targets.remove(n)
+                else:
+                    break
+
+    def detail_plan(self):
+        """
+        Build the detailed path for each drone
+        """
+
+        for d in range(self.nb_drone):
+            for s in range(1, len(self.state[d])):
+                self.plan[d] += self.mapper.paths[(self.state[d][s - 1], self.state[d][s])][0]
+
+    def write_plan(self):
+        """
+        Write the drones' paths on the grid
+        """
+        for d in range(self.nb_drone):
+            for p in self.plan[d]:
+                self.mapped_paths[p[0]][p[1]] = 2 + d
+
+    def energy(self):
+        """
+        Define the objective function
+        """
+
+        e = 0
+        for d in range(self.nb_drone):
+            e += len(self.state[d])
+
+        return e
+
+def plot_plan(plan, method, show=True):
     """
     Plot the environment
     """
@@ -112,21 +174,37 @@ def plot_plan(plan, show=True):
         plt.show()
         save = False
     if save:
-        plt.savefig('data/plot/plan/annealing_' + str(settings.X_SIZE) + 'x' + str(settings.Y_SIZE) + '.png', dpi=800)
+        plt.savefig('data/plot/plan/' + method + str(settings.X_SIZE) + 'x' + str(settings.Y_SIZE) + '.png', dpi=800)
 
 
 def get_computed_path(mapper, nb_drone):
+    #SIMULATED ANNEALING
     state = [[mapper.starting_point, mapper.starting_point] for d in range(nb_drone)]
-    pplan = SimulatedAnnealingPlanner(state, mapper, nb_drone)
-    pplan.copy_strategy = "slice"
-    pplan.steps = 50000
-    itinerary, energy = pplan.anneal()
-    pplan.detail_plan()
-    collision = pplan.check_collision()
+    saplan = SimulatedAnnealingPlanner(state, mapper, nb_drone)
+    saplan.copy_strategy = "slice"
+    saplan.steps = 50000
+    itinerary, energy = saplan.anneal()
+    saplan.detail_plan()
+    collision = saplan.check_collision()
     energy = 1 / energy
-    print(pplan.battery_plan)
+    print(saplan.battery_plan)
     print(itinerary)
     print(energy)
-    pplan.write_plan()
-    plot_plan(pplan.mapped_paths, show=True)
+    saplan.write_plan()
+    plot_plan(saplan.mapped_paths, "annealing_", show=False)
+
+    #GREEDY
+    state = [[mapper.starting_point, mapper.starting_point] for d in range(nb_drone)]
+    gplan = GreedyPlanner(state, mapper, nb_drone)
+    gplan.compute_plan()
+    gplan.detail_plan()
+    gplan.write_plan()
+    plot_plan(gplan.mapped_paths, "greedy_", show=False)
+    print(gplan.state)
+    print(gplan.battery_plan)
+    print(gplan.energy())
+
+    #RANDOM
+
+
     return 0
