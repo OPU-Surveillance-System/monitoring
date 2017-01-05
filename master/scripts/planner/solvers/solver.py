c4095364b1721d08cd63c27813e678a9522bbe7f
henriojord <henriojord@eisti.eu>
2017-01-05 17:42:08 +0900

    fix planner

--- master/scripts/planner/solvers/solver.py
+++ master/scripts/planner/solvers/solver.py
@@ -98,11 +98,13 @@ class Solver:
 
         self._build_plan()
         path = [[] for d in range(self.nb_drone)]
+        print(self.plan)
         for d in range(self.nb_drone):
             for s in range(1, len(self.plan[d])):
+                print(d, self.plan[d][s - 1], self.plan[d][s], s, len(self.plan[d]))
                 try:
                     path[d] += self.mapper.paths[(self.plan[d][s - 1], self.plan[d][s])][0]
-                except ValueError:
+                except KeyError:
                     print("No path between points: " + str(self.plan[d][s - 1]) + " and " + str(self.plan[d][s]))
             base = self.plan[d][0]
             tmp = [base]
