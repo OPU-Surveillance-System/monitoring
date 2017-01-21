a0d1fe38dea477b9bd954cfda4b2f7cb87829acb
henriojord <henriojord@eisti.eu>
2017-01-21 15:59:37 +0900

    add test cost

--- master/scripts/planner/solvers/test_cost.py
+++ master/scripts/planner/solvers/test_cost.py
@@ -11,18 +11,20 @@ mapper = pickle.load(fs)
 fs.close()
 iteration = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 750000, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000, 10000000, 11000000, 12000000, 13000000, 14000000, 15000000, 16000000, 17000000, 18000000, 19000000, 20000000]
 state = [(1059, 842), (505, 1214), (400, 1122), (502, 339), (866, 512), (1073, 82), (669, 1202), (32, 1122), (45, 52), (209, 993), (118, 653), (487, 896), (748, 638), (271, 1067), (1576, 567), (683, 316), (1483, 1156), (1448, 634), (303, 1220), (759, 823), (1614, 991), (1387, 174), (1618, 227), (367, 39), (35, 902), (967, 690), (944, 327), (912, 1029), (184, 1205), (779, 1026), (694, 123), (1502, 395)]
-nb_drone = 1
+nb_drone = 2
 rplan = UncertaintyBatteryRandomSolver(state, mapper, nb_drone)
 saplan = UncertaintyBatterySimulatedAnnealingSolver(rplan.state, mapper, nb_drone)
 for it in iteration:
     mean = []
     mean_t = []
+    mean_u = []
+    mean_d = []
     for i in range(10):
         rplan.solve()
         saplan.state = rplan.state
         saplan.copy_strategy = "slice"
         saplan.steps = it
-        saplan.Tmax = 1000
+        saplan.Tmax = 100
         saplan.Tmin = 1
         saplan.updates = 0
         t_start = time.time()
@@ -30,9 +32,13 @@ for it in iteration:
         t_end = time.time()
         mean.append(energy)
         mean_t.append(t_end - t_start)
+        mean_u.append(saplan.uncertainty_rate)
+        mean_d.append(saplan.battery_consumption)
     mean = sum(mean) / len(mean)
-    mean_t = sum(mean_t) / len (mean_t)
-    print("Test it:", it, "avg cost:", mean, "avg time:", mean_t, saplan.uncertainty_rate, saplan.battery_consumption)
+    mean_t = sum(mean_t) / len(mean_t)
+    mean_u = sum(mean_u) / len(mean_u)
+    mean_d = sum(mean_d) / len(mean_d)
+    print("Test it:", it, "avg cost:", mean, "avg time:", mean_t, "avg u:", mean_u, "avg d", mean_d)
     f = open("test_cost_res_uncertaintybattery", "a")
-    f.write(str(it) + " " + str(mean) + " " + str(mean_t) + "\n")
+    f.write(str(it) + " " + str(mean) + " " + str(mean_t) + " " + str(mean_u) + " " + str(mean_d) + "\n")
     f.close()
