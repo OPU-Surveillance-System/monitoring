10c53b4e5d6c64935b8959230c483725468a2837
henriojord <henriojord@eisti.eu>
2017-09-04 15:19:29 +0900

    change FA such that it can be called as a function, and add FA BO

--- master/scripts/planner/solvers/FireflyAlgorithm.py
+++ master/scripts/planner/solvers/FireflyAlgorithm.py
@@ -15,32 +15,18 @@ path.append("..")
 
 path.append("../..")
 import settings
+
 with open("../../webserver/data/serialization/mapper.pickle", "rb") as f:
         mapper = pickle.load(f)
 
 #open("H:\Documents\JaponStageLabo\mapper.pickle", "rb")
 #open("../../webserver/data/serialization/mapper.pickle", "rb")
 
-points = [(32, 1122), (271, 1067), (209, 993), (184, 1205), (303, 1220), (400, 1122), (505, 1214), (669, 1202), (779, 1026), (912, 1029), (1483, 1156), (1614, 991), (1576, 567), (1502, 395), (1618, 227), (1448, 634), (967, 690), (1059, 842), (759, 823), (1387, 174), (1073, 82), (944, 327), (866, 512), (748, 638), (487, 896), (118, 653), (35, 902), (502, 339), (683, 316), (694, 123), (45, 52), (367, 39)]
-
-# Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", type = int, default = 2, help = "number of drones")
-parser.add_argument("-i", type = int, default = 10000, help = "number of iterations")
-parser.add_argument("-g", type = float, default = 0.90, help = "firefly algorithm gamma")
-parser.add_argument("-a", type = float, default = 1, help = "firefly algorithm alpha")
-parser.add_argument("-f", type = int, default = 10, help = "number of fireflies")
-parser.add_argument("-e", type = float, default = 0.1, help = "distance penalization coeficient")
-parser.add_argument("-v", type = int, default = 1, help = "alpha version")
-parser.add_argument("-n", type = int, default = 1, help = "number of runs")
-args = parser.parse_args()
-if args.v == 1 or args.v == 2:
-    args.a = int(args.a)
-
 # Firefly class
 class firefly:
-    def __init__(self,  p):
-        self.eta = args.e
+    def __init__(self, p, eta, nb_drone):
+        self.eta = eta
+        self.nb_drone = nb_drone
         points = [element for element in p]
         random.shuffle(points)
         self.update(points)
@@ -58,10 +44,10 @@ class firefly:
         checkpoints = [element for element in x]
         limit = 0
         battery = 0
-        last_position = [mapper.starting_point[i] for i in range(args.d)]
+        last_position = [mapper.starting_point[i] for i in range(self.nb_drone)]
         start = [p for p in last_position]
         point_time = {}
-        drone_elapsed_time = [0 for i in range(args.d)]
+        drone_elapsed_time = [0 for i in range(self.nb_drone)]
         d = 0
         solution = []
         way = []
@@ -82,9 +68,9 @@ class firefly:
                 drone_elapsed_time[d] += path * 0.5
                 last_position[d] = start[d]
                 d += 1
-                if d >= args.d:
+                if d >= self.nb_drone:
                     d = 0
-        for d in range(args.d):
+        for d in range(self.nb_drone):
             if last_position[d] != start[d]:
                 solution.append(way)
                 path = mapper.paths[(last_position[d], start[d])][1]
@@ -98,11 +84,11 @@ class firefly:
         return mean, battery, solution
 
 # Beta step: exploitation
-def betaStep(a, b):
+def betaStep(a, b, gamma):
     a2 = [element for subList in a for element in subList]
     b2 = [element for subList in b for element in subList]
     d = distance.hamming(a2, b2)
-    beta = 1 / (1 + args.g * d)
+    beta = 1 / (1 + gamma * d)
     c = ['' for i in a2]
     toInsert = [i for i in a2]
     for i in range(len(a2)):
@@ -120,33 +106,33 @@ def betaStep(a, b):
     return c
 
 # Alpha step: exploration (v1)
-def alphaStep1(a):
-    for i in range(args.a):
+def alphaStep1(a, alpha):
+    for i in range(alpha):
         x = random.randint(0, len(a)-1)
         y = random.randint(0, len(a)-1)
         a[x], a[y] = a[y], a[x]
     return a
 
 # Alpha step: exploration (v2)
-def alphaStep2(a):
+def alphaStep2(a, alpha):
     idxs = [i for i in range(len(a))]
     random.shuffle(idxs)
-    x = idxs[0:args.a]
-    y = idxs[0:args.a]
+    x = idxs[0:alpha]
+    y = idxs[0:alpha]
     random.shuffle(y)
-    for i in range(args.a):
+    for i in range(alpha):
         a[x[i]],  a[y[i]] = a[y[i]], a[x[i]]
     return a
 
 # Alpha step: exploration (v3)
-def alphaStep3(a):
+def alphaStep3(a, alpha):
     a2 = [element for element in a]
     random.shuffle(a2)
     b2 = [element for element in a]
     c = ['' for i in a2]
     toInsert = [i for i in a2]
     for i in range(len(a2)):
-        if a2[i] == b2[i] or random.random() < args.a:
+        if a2[i] == b2[i] or random.random() < alpha:
             if a2[i] not in c:
                 c[i] = a2[i]
                 toInsert.remove(a2[i])
@@ -178,68 +164,93 @@ def plotConvergence(lehmer_hist):
     plt.clf()
 
 # Firefly algorithm
-def fireflyAlgorithm(z):
-    swarm = [firefly(points) for i in range(args.f)]
+def fireflyAlgorithm(z, **kwargs):
+    if kwargs['p']:
+        print(kwargs)
+    points = [(32, 1122), (271, 1067), (209, 993), (184, 1205), (303, 1220), (400, 1122), (505, 1214), (669, 1202), (779, 1026), (912, 1029), (1483, 1156), (1614, 991), (1576, 567), (1502, 395), (1618, 227), (1448, 634), (967, 690), (1059, 842), (759, 823), (1387, 174), (1073, 82), (944, 327), (866, 512), (748, 638), (487, 896), (118, 653), (35, 902), (502, 339), (683, 316), (694, 123), (45, 52), (367, 39)]
+    swarm = [firefly(points, kwargs['e'], kwargs['d']) for i in range(kwargs['f'])]
     swarm = sorted(swarm, key = lambda ff: ff.luminosity)
     bestFirefly = copy.deepcopy(swarm[0])
-    base = [i for i in points]
-    lehmer_hist = []
-    print("Best firefly init: ", bestFirefly.luminosity)
+    #base = [i for i in points]
+    #lehmer_hist = []
+    if kwargs['p'] == 1:
+        print("Best firefly init: ", bestFirefly.luminosity)
     tab = ([0], [bestFirefly.luminosity])
     t = 0
     n = len(swarm)
     startTime = time.time()
-    while t < args.i:
+    while t < kwargs['i']:
         for i in range(n):
             for j in range(n):
                 if j != i:
                     if swarm[j].luminosity < swarm[i].luminosity:
-                        c = betaStep(swarm[j].x,swarm[i].x)
-                        if args.v == 1:
-                            c = alphaStep1(c)
-                        elif args.v == 2:
-                            c = alphaStep2(c)
+                        c = betaStep(swarm[j].x, swarm[i].x, kwargs['g'])
+                        if kwargs['v'] == 1:
+                            c = alphaStep1(c, kwargs['a'])
+                        elif kwargs['v'] == 2:
+                            c = alphaStep2(c, kwargs['a'])
                         else:
-                            c = alphaStep3(c)
+                            c = alphaStep3(c, kwargs['a'])
                         swarm[i].update(c)
         swarm = sorted(swarm, key = lambda ff: ff.luminosity)
-        if swarm[0].luminosity == swarm[-1].luminosity:
-            print("*** swarm blocked ***")
+        if swarm[0].luminosity == swarm[-1].luminosity: #If all the fireflies are at the same position
+            if kwargs['p'] == 1:
+                print("*** swarm blocked ***")
             for i in range(1, len(swarm)):
                 c = [element for subList in swarm[i].x for element in subList]
-                if args.v == 1:
-                    c = alphaStep1(c)
-                elif args.v == 2:
-                    c = alphaStep2(c)
+                if kwargs['v'] == 1:
+                    c = alphaStep1(c, kwargs['a'])
+                elif kwargs['v'] == 2:
+                    c = alphaStep2(c, kwargs['a'])
                 else:
-                    c = alphaStep3(c)
+                    c = alphaStep3(c, kwargs['a'])
                 swarm[i].update(c)
             swarm = sorted(swarm, key = lambda ff: ff.luminosity)
         if bestFirefly.luminosity > swarm[0].luminosity:
             bestFirefly = copy.deepcopy(swarm[0])
         if t % 100 == 0:
-            print("")
-            print("Iteration: ", t)
-            print("Swarm: ", [s.luminosity for s in swarm])
-            print("Best firefly: ", bestFirefly.luminosity)
+            if kwargs['p'] == 1:
+                print("")
+                print("Iteration: ", t)
+                print("Swarm: ", [s.luminosity for s in swarm])
+                print("Best firefly: ", bestFirefly.luminosity)
             tab[0].append(t+1)
             tab[1].append(bestFirefly.luminosity)
-            lehmer_hist.append([lehmer.int_from_perm(base, [elt for subList in s.x for elt in subList]) for s in swarm])
-            plotConvergence(lehmer_hist)
+            #lehmer_hist.append([lehmer.int_from_perm(base, [elt for subList in s.x for elt in subList]) for s in swarm])
+            #plotConvergence(lehmer_hist)
         t += 1
     endTime = time.time()
-    print("Elapsed time: ", endTime - startTime)
-    displayPlot(tab,z)
+    if kwargs['p'] == 1:
+        print("Elapsed time: ", endTime - startTime)
+        displayPlot(tab,z)
+
     return bestFirefly
 
 if __name__ == "__main__":
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", type = int, default = 2, help = "number of drones")
+    parser.add_argument("-i", type = int, default = 10000, help = "number of iterations")
+    parser.add_argument("-g", type = float, default = 0.90, help = "firefly algorithm gamma")
+    parser.add_argument("-a", type = float, default = 1, help = "firefly algorithm alpha")
+    parser.add_argument("-f", type = int, default = 10, help = "number of fireflies")
+    parser.add_argument("-e", type = float, default = 0.1, help = "distance penalization coeficient")
+    parser.add_argument("-v", type = int, default = 1, help = "alpha version")
+    parser.add_argument("-n", type = int, default = 1, help = "number of runs")
+    parser.add_argument("-p", type = int, default = 1, help = "enable/desable verbose")
+    args = parser.parse_args()
+
+    if args.v == 1 or args.v == 2:
+        args.a = int(args.a)
     if not os.path.exists("plots"):
         os.makedirs("plots")
     with open("results", "w") as f:
-        print("cleaning previous results")
+        if args.p == 1:
+            print("cleaning previous results")
     for i in range(args.n):
-        bestFirefly = fireflyAlgorithm(i)
-        print("Best firefly path: ", bestFirefly.x)
-        print("Best firefly luminosity: ", bestFirefly.luminosity)
+        bestFirefly = fireflyAlgorithm(i, d=args.d, i=args.i, g=args.g, a=args.a, f=args.f, e=args.e, v=args.v, p=args.p)
+        if args.p == 1:
+            print("Best firefly path: ", bestFirefly.x)
+            print("Best firefly luminosity: ", bestFirefly.luminosity)
         with open("results", "a") as f:
             f.write("{}\n".format(bestFirefly.luminosity))
---
+++ master/scripts/planner/solvers/bo_firefly.py
@@ -0,0 +1,67 @@
+import GPy
+import GPyOpt
+import argparse
+import os
+import numpy as np
+import time
+
+import FireflyAlgorithm as ffa
+
+def func(var):
+    hist = []
+    gamma = var[:,0][0]
+    alpha = var[:,1][0]
+    fireflies = int(var[:,2][0] * 100)
+    if args.v == 1:
+        alpha = int(alpha * 16)
+    if args.v == 2:
+        alpha = int(alpha * 32)
+    for i in range(args.n):
+        best_firefly = ffa.fireflyAlgorithm(0, d=args.d, i=args.i, g=gamma, a=alpha, f=fireflies, e=args.e, v=args.v, p=args.p)
+        hist.append(best_firefly.luminosity)
+    res = np.array(hist).mean()
+    print('Tried [Gamma, Alpha, #Fireflies] = [{}, {}, {}], got {}'.format(gamma, alpha, fireflies, res))
+    with open('bayesopt', 'a') as f:
+        f.write('{}\t{}\t{}\t{}\n'.format(gamma, alpha, fireflies, res))
+
+    return res
+
+def main(args):
+    with open('bayesopt', 'w') as f:
+        print('cleaning previous results')
+    bounds = [{'name': 'gamma', 'type': 'continuous', 'domain': (0, 1)},
+              {'name': 'alpha', 'type': 'continuous', 'domain': (0, 1)},
+              {'name': 'nbfireflies', 'type': 'continuous', 'domain': (0.02, 1)}]
+    myBopt = GPyOpt.methods.BayesianOptimization(f = func,
+                                                 domain = bounds,
+                                                 model_type = 'GP',
+                                                 acquisition_type = 'EI',
+                                                 normalize_Y = True,
+                                                 exact_feval = False,
+                                                 initial_design_numdata = 8,
+                                                 evaluator_type = 'local_penalization',
+                                                 batch_size = 4,
+                                                 num_cores = 4)
+    max_iter = args.m
+    t_start = time.time()
+    myBopt.run_optimization(max_iter)
+    best_value = myBopt.fx_opt[0]
+    best_gamma = myBopt.x_opt[0]
+    best_alpha = myBopt.x_opt[1]
+    if args.v == 1:
+        best_alpha = int(best_alpha * 16)
+    if args.v == 2:
+        best_alpha = int(best_alpha * 32)
+    best_fireflies = int(myBopt.x_opt[2] * 100)
+    print('Best value: {} at [Gamma, Alpha, #Firefly] = [{}, {}, {}], found in {} s'.format(best_value, best_gamma, best_alpha, best_fireflies, time.time() - t_start))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", type = int, default = 100, help = "number of max iterations")
+    parser.add_argument("-d", type = int, default = 2, help = "number of drones")
+    parser.add_argument("-i", type = int, default = 10000, help = "number of iterations")
+    parser.add_argument("-e", type = float, default = 0.1, help = "distance penalization coeficient")
+    parser.add_argument("-v", type = int, default = 1, help = "alpha version")
+    parser.add_argument("-n", type = int, default = 1, help = "number of runs")
+    parser.add_argument("-p", type = int, default = 1, help = "enable/desable verbose")
+    args = parser.parse_args()
+    main(args)
