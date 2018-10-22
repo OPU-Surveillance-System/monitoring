8e01b0796657bd79b4802da9e2563559b2b8f610
NeTatsu <nekogui@gmail.com>
2018-10-22 18:51:10 +0900

    add bayes_opt.sh

---
+++ master/scripts/planner/solvers/bayes_opt.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5012' -bmark='Osaba_data/Osaba_50_1_2.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5013' -bmark='Osaba_data/Osaba_50_1_3.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5014' -bmark='Osaba_data/Osaba_50_1_4.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5021' -bmark='Osaba_data/Osaba_50_2_1.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5022' -bmark='Osaba_data/Osaba_50_2_2.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5023' -bmark='Osaba_data/Osaba_50_2_3.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_5024' -bmark='Osaba_data/Osaba_50_2_4.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_801' -bmark='Osaba_data/Osaba_80_1.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_802' -bmark='Osaba_data/Osaba_80_2.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_803' -bmark='Osaba_data/Osaba_80_3.xml' -v=1 -n=3
+
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5012' -bmark='Osaba_data/Osaba_50_1_2.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5013' -bmark='Osaba_data/Osaba_50_1_3.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5014' -bmark='Osaba_data/Osaba_50_1_4.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5021' -bmark='Osaba_data/Osaba_50_2_1.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5022' -bmark='Osaba_data/Osaba_50_2_2.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5023' -bmark='Osaba_data/Osaba_50_2_3.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_5024' -bmark='Osaba_data/Osaba_50_2_4.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_801' -bmark='Osaba_data/Osaba_80_1.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_802' -bmark='Osaba_data/Osaba_80_2.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_803' -bmark='Osaba_data/Osaba_80_3.xml' -v=2 -n=3
+
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_1001' -bmark='Osaba_data/Osaba_100_1.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_1002' -bmark='Osaba_data/Osaba_100_2.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v1_1003' -bmark='Osaba_data/Osaba_100_3.xml' -v=1 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_1001' -bmark='Osaba_data/Osaba_100_1.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_1002' -bmark='Osaba_data/Osaba_100_2.xml' -v=2 -n=3
+python3 bo_vrp_firefly.py -fname='vrp/bayes_opts/v2_1003' -bmark='Osaba_data/Osaba_100_3.xml' -v=2 -n=3
